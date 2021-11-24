73caccdfbafa6b279416d4ebd09137fff0df05d5
hanjf <hanjiangfeng2009@gmail.com>
2021-11-24 17:06:59 +0800

    fix: Handling special conditions

--- output.py
+++ output.py
@@ -11,6 +11,12 @@ from openpyxl import Workbook
 
 
 class Record(object):
+    def __init__(self, key='', en='', zh='', path=''):
+        self._key = key
+        self._en = en
+        self._zh = zh
+        self._path = path
+
     @property
     def key(self):
         return self._key
@@ -45,6 +51,7 @@ class Record(object):
 
 
 linePattern = re.compile(r"'(.+)':(?: +)'(.+)'(?:,?)")
+linePattern2 = re.compile(r'"(.+)":(?: +)"(.+)"(?:,?)')
 
 localesPaths = []
 enUSPaths = []
@@ -67,16 +74,37 @@ def get_locales(path):
                 get_locales(os.path.join(path, i))
 
 
+def get_files(path, lang):
+    if os.path.isfile(path):
+        if lang == 'en-US':
+            enUSPaths.append(os.path.join(path))
+        else:
+            zhCNPaths.append(os.path.join(path))
+        return
+
+    lsdir = os.listdir(path)
+    dirs = [i for i in lsdir]
+    if dirs:
+        for i in dirs:
+            if os.path.isdir(os.path.join(path, i)):
+                get_files(os.path.join(path, i), lang)
+            else:
+                if lang == 'en-US':
+                    enUSPaths.append(os.path.join(path, i))
+                else:
+                    zhCNPaths.append(os.path.join(path, i))
+
+
 def tranverse_file(path_data):
     en_dir = path_data + "\\en-US"
     if os.path.isdir(en_dir) is True:
         for j in os.listdir(en_dir):
-            enUSPaths.append(os.path.join(en_dir, j))
+            get_files(os.path.join(en_dir, j), 'en-US')
 
     zh_dir = path_data + "\\zh-CN"
     if os.path.isdir(zh_dir) is True:
         for k in os.listdir(zh_dir):
-            zhCNPaths.append(os.path.join(zh_dir, k))
+            get_files(os.path.join(zh_dir, k), 'zh-CN')
 
 
 def readContent():
@@ -87,21 +115,35 @@ def readContent():
 def readFileContent(enPath, zhPath):
     with open(enPath, mode='r', encoding='UTF-8') as files:
         for line in files:
-            if not line.strip().startswith(r'//') and linePattern.search(line):
-                arr = linePattern.split(line.strip())
-                r = Record()
-                r.key = arr[1].strip("'")
-                r.en = arr[2].strip("'")
-                r.path = enPath
-                records.append(r)
+            if not line.strip().startswith(r'//'):
+                if linePattern.search(line):
+                    arr = linePattern.split(line.strip())
+                    r = Record()
+                    r.key = arr[1].strip("'")
+                    r.en = arr[2].strip("'")
+                    r.path = enPath
+                    records.append(r)
+                elif linePattern2.search(line):
+                    arr = linePattern2.split(line.strip())
+                    r = Record()
+                    r.key = arr[1].strip("'")
+                    r.en = arr[2].strip("'")
+                    r.path = enPath
+                    records.append(r)
 
     with open(zhPath, mode='r', encoding='UTF-8') as files:
         for line in files:
-            if not line.strip().startswith(r'//') and linePattern.search(line):
-                arr = linePattern.split(line.strip())
-                rs = [i for i in records if i.key == arr[1].strip("'")]
-                if len(rs) == 1:
-                    rs[0].zh = arr[2].strip("'")
+            if not line.strip().startswith(r'//'):
+                if linePattern.search(line):
+                    arr = linePattern.split(line.strip())
+                    rs = [i for i in records if i.key == arr[1].strip("'")]
+                    if len(rs) == 1:
+                        rs[0].zh = arr[2].strip("'")
+                elif linePattern2.search(line):
+                    arr = linePattern2.split(line.strip())
+                    rs = [i for i in records if i.key == arr[1].strip("'")]
+                    if len(rs) == 1:
+                        rs[0].zh = arr[2].strip("'")
 
 
 def exportXlsx(fileName='adhub.xlsx'):
@@ -135,6 +177,7 @@ def excetue():
     exportXlsx(xlxsFile)
 
     print('---finished---')
+
 
 if __name__ == '__main__':
     excetue()
